edf746fdee44089d390bb7d22c060b1b427c8fdc
Sanam Malik <sanam.malik04@gmail.com>
2020-07-05 16:17:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ end
 def monopoly_with_third_tier
   myHash = monopoly_with_second_tier
   myHash[:railroads][:rent_in_dollars] = {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}
-  myHash[:railroads][:names] = {:reading_railroad => {}, :pennsylvania_railroa => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}
+  myHash[:railroads][:names] = {:reading_railroad => {}, :pennsylvania_railroad => 50, :b_and_o_railroad => 100, :four_pieces_owned => 200}
   return myHash
 end
 
