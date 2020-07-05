5c040c5df0b94ab97f4c9f026b1d96b27e827086
Sanam Malik <sanam.malik04@gmail.com>
2020-07-05 15:49:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,8 @@ def base_hash
 end
 
 def monopoly_with_second_tier()
-  base_hash()[:railroads] = {:pieces => 4, :rent_in_dollars => {}, :names => {}}
+  base_hash = base_hash()
+  base_hash[:railroads] = {:pieces => 4, :rent_in_dollars => {}, :names => {}}
 end
 
 def monopoly_with_third_tier
